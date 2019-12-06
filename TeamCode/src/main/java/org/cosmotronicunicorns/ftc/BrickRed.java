09e05b129deb79d8ec6c2b4e4dacc764b00e42d0
amtaidan <amtaidan@comcast.net>
2019-12-05 18:10:06 -0600

    Perfected the skystone finder and made the blue and red autonomous opmodes run off of Bot, as well as move forwards. Also changed servoGrabber, upPos, and downPos to protected variables instead of private so they could be accessed in other classes.

--- TeamCode/src/main/java/org/cosmotronicunicorns/ftc/BrickRed.java
+++ TeamCode/src/main/java/org/cosmotronicunicorns/ftc/BrickRed.java
@@ -14,6 +14,7 @@ public class BrickRed extends Bot {
         initDevices();
         waitForStart();
         boolean sFound = false;
+        boolean correctDistance = false;
         int stones = 0;
 
         while (opModeIsActive()) {
@@ -28,7 +29,7 @@ public class BrickRed extends Bot {
                 if (colorSensor.red() <= 300 && colorSensor.green() <= 550 && colorSensor.blue() <= 275) {
                     strafe(.2,50);
                     move(.25, 100);
-
+                    servoGrabber.setPosition(downPos);
                     move(.5, 250);
 
 
@@ -36,10 +37,21 @@ public class BrickRed extends Bot {
 
                 } else {
                     strafe(.5, 200);
+                    while (!correctDistance) {
+                        double distance1 = distanceSensor.getDistance(DistanceUnit.MM);
+                        if (distance1 > 30) {
+                            move(.25);
+                        } else if(distance1 < 30) {
+                            move(-.25);
+                        } else {
+                            correctDistance = true;
+                        }
+                    }
                     stones += 1;
                 }
 
                 strafe(-.5, (stones * 200) + 4050);
+                servoGrabber.setPosition(upPos);
             }
         }
     }
