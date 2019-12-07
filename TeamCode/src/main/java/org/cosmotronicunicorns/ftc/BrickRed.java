77877a3b1433d6918801bd73c33019f099b84593
Fredrik Breivald <fredrik@tegus.co>
2019-12-07 15:58:52 -0600

    Made functions for the yellow color and to put the arm down and wait until it's fully down. Also made a lot of tweaks to the skystone finder code.

--- TeamCode/src/main/java/org/cosmotronicunicorns/ftc/BrickRed.java
+++ TeamCode/src/main/java/org/cosmotronicunicorns/ftc/BrickRed.java
@@ -7,7 +7,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 @Autonomous
 public class BrickRed extends Bot {
 
-    private static double POWER = 0.2;
+    private static double POWER = 0.15;
+    private static double STRAFEPOWER = 0.15;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -20,39 +21,54 @@ public class BrickRed extends Bot {
         while (opModeIsActive()) {
             while(true) {
                 double distance = distanceSensor.getDistance(DistanceUnit.MM);
-                if (!(distance > 30)) break;
                 telemetry.addData("Distamce (mm):", distance);
+                telemetry.update();
+                if (distance < 48) break;
                 move(POWER);
             }
             halt();
-            while (!sFound) {
-                if (colorSensor.red() <= 300 && colorSensor.green() <= 550 && colorSensor.blue() <= 275) {
-                    strafe(.2,50);
-                    move(.25, 100);
-                    servoGrabber.setPosition(downPos);
-                    move(.5, 250);
+            double startTime =getRuntime();
 
-
-                    sFound = true;
-
-                } else {
-                    strafe(.5, 200);
-                    while (!correctDistance) {
-                        double distance1 = distanceSensor.getDistance(DistanceUnit.MM);
-                        if (distance1 > 29) {
-                            move(.25);
-                        } else if(distance1 < 31) {
-                            move(-.25);
-                        } else {
-                            correctDistance = true;
-                        }
-                    }
-                    stones += 1;
-                }
-
-                strafe(-.5, (stones * 200) + 4050);
-                servoGrabber.setPosition(upPos);
+            while(!yellow()) {
+                telemetry.addData("color:", "%d, %d, %d", colorSensor.red(), colorSensor.green(), colorSensor.blue());
+                telemetry.update();
+                move(.1);
             }
+            halt();
+            move(-.25,30);
+            sleep(2000);
+            while (!sFound) {
+                telemetry.addData("color:", "%d, %d, %d", colorSensor.red(), colorSensor.green(), colorSensor.blue());
+                telemetry.update();
+                if (!yellow()) {
+                    sFound = true;
+                    halt();
+                } else {
+                    strafe(-STRAFEPOWER, 200);
+                    strafe(-STRAFEPOWER);
+                }
+            }
+            halt();
+            double endTime = getRuntime();
+            strafe(.2,150);
+            move(.25, 200);
+            setGrabberPos(downPos);
+            sleep(1000);
+            move(-.5, 500);
+            strafe(STRAFEPOWER , ((int)((endTime - startTime) * 1000)) + 4050);
+            setGrabberPos(upPos);
+        }
+    }
+
+    boolean yellow () {
+        return colorSensor.red() > 25000 && colorSensor.green() > 25000 && colorSensor.blue() < 10000;
+
+    }
+
+    void setGrabberPos(double pos) {
+        servoGrabber.setPosition(pos);
+        while (Math.abs(servoGrabber.getPosition() - pos) > 0.01) {
+            sleep(50);
         }
     }
 }
