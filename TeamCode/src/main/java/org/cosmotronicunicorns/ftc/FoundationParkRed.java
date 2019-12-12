106e41da66de85192e13a797dddc33aeab0bcc7d
Fredrik Breivald <fredrik@tegus.co>
2019-12-11 19:56:38 -0600

    Skyblock finding works with also moving the foundation into the build zone and parking. Have not tested blue yet. Also made two modes starting on the closer side to the foundation where it does not find the skystone, it only moves the foundation and parks. Made for blue are red.

---
+++ TeamCode/src/main/java/org/cosmotronicunicorns/ftc/FoundationParkRed.java
@@ -0,0 +1,43 @@
+package org.cosmotronicunicorns.ftc;
+
+import android.graphics.Color;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.robotcore.hardware.Gyroscope;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit.MM;
+
+@Autonomous
+public class FoundationParkRed extends Bot {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        initDevices();
+        waitForStart();
+
+        // Actual commands using functions declared lines 24 - 60
+        move(.5,100);
+        strafe(.35, 3000);
+        strafe(-.35, 500);
+        move(.5, 400);
+        while(opModeIsActive() && distanceSensor.getDistance(MM) > 11) {
+            move(.2);
+        }
+        servoGrabber.setPosition(downPos+.05);
+        sleep(1000);
+        move(-.25, 5000);
+        servoGrabber.setPosition(upPos);
+        strafe(-1, 900);
+
+
+    }
+
+
+}
+
